fb9103a4f76aa756583c3f1729f7935ca4893e5d
Mitesh Mankame (Contractor) <Mankame.Mitesh@azdeq.gov>
2020-07-06 15:45:40 -0700

    Added Badge dts

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,6 +2,15 @@ console.log('Power up Loaded')
 window.TrelloPowerUp.initialize({
     'card-badges': function(t){
         //Return card badges
-        return []
+        return t.card('all')
+        .then(function(card){
+            console.log(card)
+            return [
+                {
+                    text:card.idShort
+                }
+            ]
+        })
+        
     }
 })
