9d4a5a6d4e6860a0f0b9c5c2ed06f013da0f920a
Mitesh Mankame (Contractor) <Mankame.Mitesh@azdeq.gov>
2020-07-06 16:29:37 -0700

    Added cros origin policy

--- src/js/connector.js
+++ src/js/connector.js
@@ -8,7 +8,7 @@ window.TrelloPowerUp.initialize({
 
             if(card.coordinates){
                 // fetch weather data
-                fetch(`https://samples.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lng=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`)
+                fetch(`https://samples.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lng=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`, {'mode': 'no-cors'})
                 .then(response => response.json())
                 .then(weatherData =>{
                     return [{
