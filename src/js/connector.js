e1df9b9bd55eaa5187943caa5d9536c8e6f8cb3a
Mitesh Mankame (Contractor) <Mankame.Mitesh@azdeq.gov>
2020-07-07 12:58:10 -0700

    Added Cache

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,4 +1,44 @@
+const promise = window.TrelloPowerUp.Promise;
+
 const getWeatherData=(t)=>{
+    return promise.all([t.card('coordinates'),t.get('card', 'shared', 'locationDetails')])
+    .spread((card,cacheData)=>{
+        if(!card.coordinates) return [];
+
+        if(cacheData &&  
+           cacheData.coordinates.latitude == card.coordinates.latitude && 
+           cacheData.coordinates.longitude == card.coordinates.longitude &&
+           cacheData.weatherdata &&
+           cacheData.expires >= Date.now()){
+            console.log('From Cache',cacheData.weatherdata)
+               return cacheData.weatherdata;
+           }
+
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lon=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`)
+        .then(response => response.json())
+        .then(weatherData =>{  
+            const temprature=  (weatherData.main.temp - 273.15) * 9/5 + 32;
+            weatherData.main.formattedTemprature =  `${temprature} °F`;
+
+            if(t.memberCanWriteToModel('card')){               
+            t.set('card', 'shared', 'locationDetails',{
+                coordinates:{
+                             latitude:card.coordinates.latitude,
+                             longitude:card.coordinates.longitude,
+                            },
+                expires: Date.now() + (1000 * 60 * 30),
+                weatherdata:weatherData
+            })
+        }
+        console.log('From Serive ',weatherdata)
+            return weatherData
+        });
+
+    })
+
+
+
+
 
     return t.card('coordinates')
         .then(function(card){        
@@ -6,8 +46,9 @@ const getWeatherData=(t)=>{
                 // fetch weather data
                 return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lon=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`)
                 .then(response => response.json())
-                .then(weatherData =>{    
-                    weatherData.main.formattedTemprature =  `${(weatherData.main.temp - 273.15) * 9/5 + 32} °F`
+                .then(weatherData =>{  
+                    const temprature=  (weatherData.main.temp - 273.15) * 9/5 + 32;
+                    weatherData.main.formattedTemprature =  `${temprature} °F`
                     return weatherData
                 });
             }
