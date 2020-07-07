396879faf06229d814a5a34a44c173ba1ed67c3c
Mitesh Mankame (Contractor) <Mankame.Mitesh@azdeq.gov>
2020-07-07 12:21:33 -0700

    Refactor-2

--- src/js/connector.js
+++ src/js/connector.js
@@ -19,7 +19,7 @@ getWeatherDataBadges = (weatherData)=>{
     if(!weatherData) return[];
     return  [{
                     title:'Temprature',
-                    text:`${weatherData.main.formattedTemprature}`
+                    text:weatherData.main.formattedTemprature
                 },
                 {
                     title:'Wind Speed',
@@ -42,34 +42,7 @@ window.TrelloPowerUp.initialize({
    
       'card-detail-badges': function(t){
         //Return card badges
-        return t.card('coordinates')
-        .then(function(card){        
-            if(card.coordinates){               
-                // fetch weather data
-                return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lon=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`)
-                .then(response => response.json())
-                .then(weatherData =>{
-                   // console.log('Weather Data',weatherData)
-                   let temprature = (weatherData.main.temp - 273.15) * 9/5 + 32;
-                    let returnArr = [{
-                        title:'Temprature',
-                        text:`${temprature.toFixed()} °F`
-                    },
-                    {
-                        title:'Wind Speed',
-                        text:`🌬️${weatherData.wind.speed.toString()} m/h`
-                    },
-                    {
-                        title:'Conditions',
-                        icon:`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
-                        text:weatherData.weather[0].main
-                    }
-                ]
-                  
-                    return returnArr
-                });
-            }
-            return [];           
-        });        
-    }
+        return  getWeatherData(t)
+        .then(getWeatherDataBadges);
+      }
 });
