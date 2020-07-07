13b81621ecaecbe4e4865d79714ea65763fa3332
Mitesh Mankame (Contractor) <Mankame.Mitesh@azdeq.gov>
2020-07-07 09:28:31 -0700

    Added debug logs

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,13 +4,14 @@ window.TrelloPowerUp.initialize({
         //Return card badges
         return t.card('all')
         .then(function(card){
-            console.log('card.pos:',card.pos)
+            /*console.log('card.pos:',card.pos)
             return [
                 {
                     text:card.pos
                 }
-            ]
-           /* if(card.coordinates){
+            ]*/
+            if(card.coordinates){
+                console.log('Lat: '+card.coordinates.latitude,"Lng : "+card.coordinates.longitude)
                 // fetch weather data
                 fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lon=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`)
                 .then(response => response.json())
@@ -18,13 +19,17 @@ window.TrelloPowerUp.initialize({
                     console.log('Weather Data',weatherData)
                     return [
                     {
-                        text:card.pos
+                        text:card.coordinates.longitude.toString()
                     }
                 ]
                 });
             }else{
-                return []
-            } */
+                return [
+                    {
+                        text:card.pos
+                    }
+                ]
+            }
 
            
         })
