8b445b4892670605922aabadcefc395fa6185290
Mitesh Mankame (Contractor) <Mankame.Mitesh@azdeq.gov>
2020-07-07 12:16:49 -0700

    Refactor -1

--- src/js/connector.js
+++ src/js/connector.js
@@ -6,59 +6,37 @@ const getWeatherData=(t)=>{
                 // fetch weather data
                 return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lon=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`)
                 .then(response => response.json())
-                .then(weatherData =>{
-                   // console.log('Weather Data',weatherData)
-                   let temprature = (weatherData.main.temp - 273.15) * 9/5 + 32;
-                    let returnArr = [{
-                        text:`${temprature.toFixed()} °F`
-                    },
-                    {
-                        text:`🌬️${weatherData.wind.speed.toString()} m/h`
-                    },
-                    {
-                        icon:`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
-                        text:weatherData.weather[0].main
-                    }
-                ]
-                  
-                    return returnArr
+                .then(weatherData =>{    
+                    weatherData.main.formattedTemprature =  `${(weatherData.main.temp - 273.15) * 9/5 + 32} °F`
+                    return weatherData
                 });
             }
-            return [];           
+            return null;           
         });
+}
+
+getWeatherDataBadges = (weatherData)=>{
+    if(!weatherData) return[];
+    return  [{
+                    title:'Temprature',
+                    text:`${temprature.toFixed()} °F`
+                },
+                {
+                    title:'Wind Speed',
+                    text:`🌬️${weatherData.wind.speed.toString()} m/h`
+                },
+                {
+                    title:'Conditions',
+                    icon:`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
+                    text:weatherData.weather[0].main
+                }
+            ];
 }
 
 window.TrelloPowerUp.initialize({
     'card-badges': function(t){
-        //Return card badges
-        return t.card('coordinates')
-        .then(function(card){        
-            if(card.coordinates){               
-                // fetch weather data
-                return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lon=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`)
-                .then(response => response.json())
-                .then(weatherData =>{
-                   // console.log('Weather Data',weatherData)
-                   let temprature = (weatherData.main.temp - 273.15) * 9/5 + 32;
-                    let returnArr = [{
-                       
-                        text:`${temprature.toFixed()} °F`
-                    },
-                    {
-                     
-                        text:`🌬️${weatherData.wind.speed.toString()} m/h`
-                    },
-                    {
-                        
-                        icon:`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
-                        text:weatherData.weather[0].main
-                    }
-                ]                  
-                    return returnArr
-                });
-            }
-            return [];           
-        });        
+        return getWeatherData(t)
+        .then(getWeatherDataBadges);
     },
 
    
