c427a1451b544971935078896d6da9c9f0d1bab0
Mitesh Mankame (Contractor) <Mankame.Mitesh@azdeq.gov>
2020-07-07 11:05:06 -0700

    Added details to card badges

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,8 +1,6 @@
+const getWeatherData=(t)=>{
 
-window.TrelloPowerUp.initialize({
-    'card-badges': function(t){
-        //Return card badges
-        return t.card('coordinates')
+    return t.card('coordinates')
         .then(function(card){        
             if(card.coordinates){               
                 // fetch weather data
@@ -27,20 +25,44 @@ window.TrelloPowerUp.initialize({
                 });
             }
             return [];           
+        });
+}
+
+window.TrelloPowerUp.initialize({
+    'card-badges': function(t){
+        //Return card badges
+        return t.card('coordinates')
+        .then(function(card){        
+            if(card.coordinates){               
+                // fetch weather data
+                return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lon=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`)
+                .then(response => response.json())
+                .then(weatherData =>{
+                   // console.log('Weather Data',weatherData)
+                   let temprature = (weatherData.main.temp - 273.15) * 9/5 + 32;
+                    let returnArr = [{
+                        title:'Temprature',
+                        text:`${temprature.toFixed()} °F`
+                    },
+                    {
+                        title:'Wind Speed',
+                        text:`🌬️${weatherData.wind.speed.toString()} m/h`
+                    },
+                    {
+                        title:'Conditions',
+                        icon:`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
+                        text:weatherData.weather[0].main
+                    }
+                ]
+                  
+                    return returnArr
+                });
+            }
+            return [];           
         });        
     },
 
-    'card-back-section': function(t, options){
-        return {
-          title: 'My Card Back Section',
-          icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg', // Must be a gray icon, colored icons not allowed.
-          content: {
-            type: 'iframe',
-            url: t.signUrl('../html/section.html'),
-            height: 230 // Max height is 500
-          }
-        };
-      },
+   
       'card-detail-badges': function(t){
         //Return card badges
         return t.card('coordinates')
