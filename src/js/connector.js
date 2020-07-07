79156cfadb55dfe275dd9da26955cacb947c5c5d
Mitesh Mankame (Contractor) <Mankame.Mitesh@azdeq.gov>
2020-07-07 09:52:03 -0700

    Code cleanup to show temp and wind details

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,33 +2,26 @@
 window.TrelloPowerUp.initialize({
     'card-badges': function(t){
         //Return card badges
-        return t.card('all')
-        .then(function(card){
-            /*console.log('card.pos:',card.pos)
-            return [
-                {
-                    text:card.pos
-                }
-            ]*/
-            if(card.coordinates){
-                console.log('Lat: '+card.coordinates.latitude,"Lng : "+card.coordinates.longitude)
+        return t.card('coordinates')
+        .then(function(card){        
+            if(card.coordinates){               
                 // fetch weather data
                 return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${card.coordinates.latitude}&lon=${card.coordinates.longitude}&appid=%%WEATHER_KEY%%`)
                 .then(response => response.json())
                 .then(weatherData =>{
-                    console.log('Weather Data',weatherData)
+                   // console.log('Weather Data',weatherData)
                    
                     let returnArr = [{
-                        text:card.coordinates.longitude.toString()
+                        text:weatherData.main.temp.toString()
+                    },
+                    {
+                        text:weatherData.wind.speed.toString()
                     }]
-                    console.log('First: ',returnArr)
+                  
                     return returnArr
                 });
             }
-            return [];
-           
-        })
-      
-        
+            return [];           
+        });        
     }
-})
+});
